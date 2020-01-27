2d749e9d9c8b593ba52129ad0f9284de4bedcb75
saradajani <37843528+saradajani@users.noreply.github.com>
2020-01-27 13:25:47 -0800

    Add files via upload

---
+++ lilt.rb
@@ -0,0 +1,103 @@
+use_bpm 92
+use_synth :piano
+
+sample "C:/Users/sara_dajani/Downloads/challenge_d/idk.wav"
+sleep 2.5
+play :g3, sustain: 5
+play :e4, sustain: 5
+play :d4
+sleep 2.5
+sample "C:/Users/sara_dajani/Downloads/challenge_d/idkk.wav"
+sleep 2
+play :c5
+sleep 0.25
+play :d5
+sleep 1.5
+play :d4
+play :a3
+play :f3, sustain: 5
+sleep 1.25
+play :b4
+sleep 0.75
+play :a4
+sleep 0.25
+play :b4
+sleep 1.5
+play :d4
+play :b3
+play :f3, sustain:5
+sleep 2
+play :c4
+play :b3
+play :g3, sustain:5
+sleep 0.5
+play :d4
+play :a3
+play :f3
+sleep 1
+play :b4
+sleep 0.5
+play :a4
+sleep 0.25
+play :a4
+sleep 0.75
+play :g3, sustain: 5
+play :e4, sustain: 5
+play :d4
+sleep 2.5
+play :d5
+sleep 1
+play :d4
+play :a3
+play :f3, sustain: 5
+sleep 0.75
+play :b4
+sleep 2.5
+play :a4
+sleep 0.25
+play :c4
+sleep 0.25
+play :b4
+sleep 1
+play :d4
+play :b3
+play :f3, sustain:5
+sleep 2
+play :c4
+play :b3
+play :g3, sustain:5
+sleep 2
+play :g3, sustain: 5
+play :e4, sustain: 5
+play :d4
+sleep 0.5
+play :d4
+sleep 0.25
+play :d4
+sleep 0.25
+play :d4
+sleep 0.5
+play :b4
+sleep 0.5
+play :g4
+sleep 0.5
+play :g4
+sleep 0.25
+play :a4
+sleep 0.25
+play :b4
+sleep 0.5
+play :a4
+sleep 0.5
+play :b4
+sleep 0.25
+play :b4
+sleep 0.25
+play :b4
+sleep 0.25
+play :b4
+sleep 0.25
+play :b4
+sleep 0.25
+play :b4
+sleep 0.25
